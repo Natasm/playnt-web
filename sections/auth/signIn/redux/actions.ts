eadc214e1fc9db3ae4bfacfc7606f6d0af8b4739
Natan Morais <natasantanademorais@gmail.com>
2022-12-30 11:59:43 -0300

    Fix login redux

--- sections/auth/signIn/redux/actions.ts
+++ sections/auth/signIn/redux/actions.ts
@@ -2,22 +2,22 @@ import { setCookie } from "cookies-next"
 import { signIn } from "next-auth/react"
 import { Dispatch } from "redux"
 import { setLoadingReducer } from "../../../../redux/actions"
-import { login as loginService } from "../../../../services/auth"
+import { login } from "../../../../services/auth"
 
-export const loginAction = (login: string, password: string) => {
+export const loginAction = (email: string, password: string) => {
     return async function (dispatch: Dispatch) {
         try {
             
             dispatch(setLoadingReducer(true))
 
-            const response = await loginService(login, password)
+            const response = await login(email, password)
 
             if (response?.data?.token && response?.data?.customerId) {
 
                 setCookie('user-token', response.data.token)
 
                 await signIn('signIn', {
-                    login,
+                    email,
                     callbackUrl: '/home'
                 })
             }
